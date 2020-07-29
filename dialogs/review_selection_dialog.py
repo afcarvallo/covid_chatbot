b0b21a25e08874e6a6610533c7c03d5e4f7e24c1
afcarvallo <afcarvallo@uc.cl>
2020-07-28 22:09:18 -0400

    keys update

--- dialogs/review_selection_dialog.py
+++ dialogs/review_selection_dialog.py
@@ -7,10 +7,10 @@ import tweepy
 from collections import Counter 
 
 ## tweepy authentication
-access_token = "153003474-CO4SaZ8H0NfShJ2iuZVZnn2ukxyhaIE0oUQTsWXu"
-access_token_secret = "sxJhJZnrcJNTIx8hz5Zguz16g5szkLXxuI68sC7DBvPrR"
-consumer_key = "Gqvnu2yKYFsxBiOlM6lx5GEfA"
-consumer_secret = "1T7axrJ7CvobNR6F2gjXBjFmZ61jkdg3Vjddm3E4ywUziiTDoZ"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -22,8 +22,8 @@ api = tweepy.API(auth)
 url = "https://covid-193.p.rapidapi.com/statistics"
 
 headers = {
-    'x-rapidapi-host': "covid-193.p.rapidapi.com",
-    'x-rapidapi-key': "ff429794cdmsh07f4e03f81756efp1560b5jsnb34cb32bf306"
+    'x-rapidapi-host': "",
+    'x-rapidapi-key': ""
     }
 
 from typing import List
